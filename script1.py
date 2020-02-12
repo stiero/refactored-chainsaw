9880492bed5aa259ff38d37486877bfb7d37c82d
stiero <hensontauro@yahoo.com>
2020-02-12 17:58:59 +0530

    .

--- script1.py
+++ script1.py
@@ -22,7 +22,7 @@ lemmatizer = WordNetLemmatizer()
 
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
-stop_words.extend(["minutes", "hours", "seconds", "teaspoon"])
+stop_words.extend(["minutes", "hours", "seconds", "teaspoon", "finely", "thinly", "briskly", "occasinally", "gently"])
 
 import string
 punctuations = '!"#$%&\'()*+,.-/:;<=>?@[\\]^_`{|}~'
@@ -47,6 +47,9 @@ df = df.where(pd.notnull(df), None)
 
 
 df = df[df.duplicated() == False]
+
+#Random shuffle
+df = df.sample(frac=1, random_state=1234)
 
 
 cooking_method = df['cooking_method']
@@ -92,7 +95,7 @@ nlp = spacy.load("en_core_web_lg")
 
 columns = list(df.columns)
 
-aa = []
+tok_cooking_method = []
 
 for elem in tqdm(cooking_method):
     elem = elem.translate(table)
@@ -100,7 +103,7 @@ for elem in tqdm(cooking_method):
 
     elem = word_tokenize(elem)
     # elem = [word.translate(puncts) for word in elem]
-    elem = [word.lower() for word in elem if word not in stop_words]
+    elem = [word.lower() for word in elem if word not in stop_words and word.isalpha() and not word.endswith("ly")]
     
     # elem = ' '.join(elem)
 
@@ -108,11 +111,11 @@ for elem in tqdm(cooking_method):
     #     if elem[i] == '' and elem[i+1] == '':
     #         del elem[i+1]
     
-    aa.append(elem)
+    tok_cooking_method.append(elem)
 
 
 
-bb = []
+tok_ingredients = []
 
 for elem in tqdm(ingredients):
     elem = elem.translate(table)
@@ -120,7 +123,7 @@ for elem in tqdm(ingredients):
     # elem = elem.split(",")
     elem = word_tokenize(elem)
     # elem = [word.translate(puncts) for word in elem]
-    elem = [word.lower() for word in elem if word not in stop_words]
+    elem = [word.lower() for word in elem if word not in stop_words and word.isalpha() and not word.endswith("ly")]
     
     # elem = ' '.join(elem)
 
@@ -128,12 +131,46 @@ for elem in tqdm(ingredients):
     #     if elem[i] == '' and elem[i+1] == '':
     #         del elem[i+1]
     
-    bb.append(elem)
+    tok_ingredients.append(elem)
 
 
 
+#Length of text
+    
+len_cooking = []
+for subl in tok_cooking_method:
+    len_cooking.append(len(subl))
+
+max(len_cooking)
+
+len_ingredients = []
+
+for subl in tok_ingredients:
+    len_ingredients.append(len(subl))
+    
+max(len_ingredients)
 
 
+
+import seaborn as sns
+sns.kdeplot(len_cooking)
+sns.kdeplot(len_ingredients)
+
+
+
+for i, subl in enumerate(tok_cooking_method):
+    subl = subl[:200]
+    tok_cooking_method[i] = subl
+    
+for i, subl in enumerate(tok_ingredients):
+    subl = subl[:100]
+    tok_ingredients[i] = subl
+
+
+tok_concat = []
+
+for i, j in zip(tok_cooking_method, tok_ingredients):
+    tok_concat.append(i + j)
 
 
 
@@ -183,3 +220,139 @@ for elem in tqdm(cooking_method):
 
     
     
+    
+import keras.backend as K
+import numpy as np
+    
+total_size = len(len_cooking)
+    
+train_indices = list(range(0,int(total_size*0.8)-1))
+train_size = len(train_indices)
+
+val_indices = list(range(max(train_indices), total_size))
+val_size = len(val_indices)
+
+max_text_len = 300
+vector_size = 100
+
+X_train = np.zeros((train_size, max_text_len, vector_size), dtype=K.floatx())
+
+X_val = np.zeros((val_size, max_text_len, vector_size), dtype=K.floatx())
+
+
+for index in range(0, total_size):
+    for t, token in enumerate(tok_concat[index]):
+        if t >= max_text_len:
+            break
+        
+        
+        if index < train_size:
+            X_train[index, t, :] = vecs[token]
+            
+        else:
+            X_val[index-train_size, t, :] = vecs[token]
+            
+            
+
+
+ntags = len(tags_unique)
+
+tags2id = {tag:i for i, tag in enumerate(tags_unique)}
+
+#y_train = [[0]*len(tags_unique)]*total_size
+
+y = np.zeros((total_size, ntags), dtype=np.int8)
+
+for i, tag_list in enumerate(tags):
+    for j, tag in enumerate(tag_list):
+        if tag in tags2id.keys():
+            k = tags2id[tag]
+            y[i][k] = 1
+        
+        # for k in tags2id.values():
+        #     if j == tags2id[tag]:
+        #         y_train[i][k] = 1
+
+
+# from keras.utils import to_categorical
+
+# y_train = []
+
+# for tag_list in tags:
+#     y = [to_categorical(tags2id[i], num_classes=ntags) for i in tag_list]
+#     y_train.append(y)
+    
+
+y_train = y[:train_size]
+y_val = y[train_size-1:]
+
+
+
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+from keras.callbacks import EarlyStopping
+from keras.models import Input, Model
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers import Embedding, Bidirectional, LSTM
+from keras.layers.convolutional import Conv1D
+from keras.optimizers import Adam
+
+from keras.losses import sparse_categorical_crossentropy
+
+import tensorflow as tf
+
+
+batch_size = 32
+nb_epochs = 1
+
+input_ = Input(shape=(max_text_len,vector_size))
+
+model = Bidirectional(LSTM(units=100, recurrent_dropout=0.1))(input_)
+out = Dense(ntags, activation="softmax")(model)
+model = Model(input_, out)
+
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(lr=0.0001, decay=1e-6),
+              metrics=['accuracy'])
+
+history = model.fit(X_train, y_train,
+              batch_size=batch_size,
+              shuffle=True,
+              epochs=nb_epochs,
+              validation_data=(X_val, y_val),
+              callbacks=[EarlyStopping(min_delta=0.00025, patience=2)],
+                verbose=1,
+                class_weight = tag_weights)
+
+
+
+
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+
+tot_tags = len(tags_flat)
+tag_weights = dict(Counter(tags_flat))
+
+tag_weights = {tags2id[k]:tot_tags/v for k,v in tag_weights.items()}
+
+
+
+
+from scipy import spatial
+
+1 - spatial.distance.cosine(vecs['pasta'], vecs['linguini'])
+
